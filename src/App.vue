99a0d56e23e8088997475b8de8de3faee6b833e3
Jonian Guveli <jonian@hardpixel.eu>
2018-07-19 21:17:32 +0300

    rename project to vessel

--- src/App.vue
+++ src/App.vue
@@ -14,5 +14,5 @@
   @import url("https://fonts.googleapis.com/css?family=Open+Sans:300,400,600");
   @import url("https://cdn.materialdesignicons.com/2.3.54/css/materialdesignicons.min.css");
 
-  @import "scss/bedrock.scss";
+  @import "scss/vessel.scss";
 </style>
