d907a0fb56c76ccaefe528f05e93bf30d9b18a59
Jonian Guveli <jonian@hardpixel.eu>
2018-07-21 15:21:32 +0300

    add media component

---
+++ src/components/Media.vue
@@ -0,0 +1,38 @@
+<template>
+  <figure>
+    <img :src="src" :alt="alt">
+  </figure>
+</template>
+
+<script>
+  export default {
+    name: 'media',
+    props: {
+      src: {
+        type: String,
+        default: null
+      },
+      alt: {
+        type: String,
+        default: null
+      }
+    },
+    data () {
+      return {
+
+      }
+    },
+    watch: {
+
+    },
+    mounted () {
+
+    },
+    computed: {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import "normalize.css/normalize.css";
 import Vue from 'vue'
 import App from './App.vue'
 
+import Media from './components/Media.vue'
 import Card from './components/Card.vue'
 import Tabs from './components/Tabs.vue'
 
@@ -11,6 +12,7 @@ import store  from './store'
 
 Vue.config.productionTip = false
 
+Vue.component('media', Media)
 Vue.component('card', Card)
 Vue.component('tabs', Tabs)
 
